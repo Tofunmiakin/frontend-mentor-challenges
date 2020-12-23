d0f79812962b2c8adbbfe5bfdf45f60d8d0ae3d6
R04DM4N <58348842+Tofunmiakin@users.noreply.github.com>
2020-12-23 18:40:32 +0100

    abandoned

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,17 +1,14 @@
 function accordionDrop(id) {
   var x = document.getElementById(id);
+  // var y = document.getElementById(id2);
   if (x.className.indexOf("panel") == -1) {
     x.className += "panel";
+    // y.style.fontWeight = "700"
   } else {
     x.className = x.className.replace("panel", "");
   }
 }
 
-function changeFont() {
-
-  document.getElementsByClassName('accordion').style.fontWeight = "700";
-  
-}
 
 
 
